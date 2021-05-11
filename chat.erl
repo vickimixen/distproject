1ab9f8d6d9b42ca7717a45e8d4e81a56debb4d82
Andreas Lyngholm Poulsen <Lyngholmdk@gmail.com>
2021-05-11 14:04:48 +0200

    Start

--- chat.erl
+++ chat.erl
@@ -1,11 +1,17 @@
 -module(chat).
 
+%-export([start/0,stop/0,put/2,get/1,remove/1,size/0,create_slaves/1,remove_slaves/1, exit_master/0]).
+-export_type([users/0, messages/0, channels/0]).
+-export_opaque(key/0).
+
+-type( key() :: non_neg_integer() ).
+
 -record(user, {
 	name :: string()
 }).
 
 -record(message, {
-	user :: user(),
+	user :: user,
 	text :: string()
 }).
 
@@ -14,15 +20,17 @@
 
 -record(channel, {
 	key :: pid(),
-  	users :: users(),
-  	messages :: messages()
+	users :: users(),
+	messages :: messages()
 }).
 
+-opaque(channels() :: map()).
 
 
 -record(node,{
   key :: pid(),
-  pid :: key()
+  pid :: key(),
+  channels :: channels()
 }).
 
 -record(state,{
@@ -31,3 +39,13 @@
   predecessor = undefined :: undefined | #node{},
   predecessor_monitor :: reference()
 }).
+
+
+-spec start() -> #node{}.
+start() ->
+  P = spawn(fun() ->
+    Self = #node{ key = hash(self()) , pid = self() },
+    spawn_link( fun() -> stabilise(Self,Self) end),
+    loop(#state{ self = Self, successor = Self })
+  end),
+  #node{ key = hash(P) , pid = P }.
