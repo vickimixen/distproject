5f3970b9187f9e65ceef692da014472ce04b5c77
Andreas Lyngholm Poulsen <Lyngholmdk@gmail.com>
2021-05-11 08:32:37 +0200

    Added initial idea for data structure

--- chat.erl
+++ chat.erl
@@ -1,1 +1,33 @@
-%ikke lavet noget endnu
+-module(chat).
+
+-record(user, {
+	name :: string()
+}).
+
+-record(message, {
+	user :: user(),
+	text :: string()
+}).
+
+-opaque(users() :: map()).
+-opaque(messages() :: map()).
+
+-record(channel, {
+	key :: pid(),
+  	users :: users(),
+  	messages :: messages()
+}).
+
+
+
+-record(node,{
+  key :: pid(),
+  pid :: key()
+}).
+
+-record(state,{
+  self :: #node{},
+  successor :: #node{},
+  predecessor = undefined :: undefined | #node{},
+  predecessor_monitor :: reference()
+}).
